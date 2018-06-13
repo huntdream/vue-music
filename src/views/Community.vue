f3473dca02a7a2734596f79248b42fa203f026af
maoyu <maoyu960320@hotmail.com>
2018-06-13 15:52:58 +0800

    format code

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-    name:'post'
+  name: 'post'
 }
 </script>
--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,9 +1,22 @@
 <template>
+  <div>
     <h1>My View</h1>
+    <button @click="say()">Say</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'me'
+  name: 'me',
+  data() {
+    return {
+      name: 'hello'
+    }
+  },
+  methods: {
+    say() {
+      console.log("hello")
+    }
+  }
 }
 </script>
