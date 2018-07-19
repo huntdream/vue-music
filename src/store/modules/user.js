1a0efe4cc1702b53e53133ebbbbe05245da37450
huntdream <maoyu960320@hotmail.com>
2018-07-19 22:03:54 +0800

    intergrated with vuex

---
+++ src/store/modules/user.js
@@ -0,0 +1,25 @@
+import { userdetail, userplaylist } from '../../api/index'
+
+export default {
+  namespaced: true,
+  state: {
+    detail: [],
+    playlist: []
+  },
+  mutations: {
+    saveDetail(state, detail) {
+      state.detail = detail
+    },
+    savePlaylist(state, playlist) {
+      state.playlist = playlist
+    }
+  },
+  actions: {
+    detail({ commit }) {
+      return userdetail().then(res => commit('saveDetail', res))
+    },
+    playlist({ commit }) {
+      return userplaylist().then(res => commit('savePlaylist', res))
+    }
+  }
+}
