49c621b4eedb10d958429923e918d8c8fd69a028
huntdream <maoyu960320@hotmail.com>
2018-07-19 22:06:16 +0800

    capitalize components name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@
 
 <script>
   export default {
-    name: 'navbar',
+    name: 'Navbar',
     data() {
       return {
         isAlpha: false
--- src/views/Music.vue
+++ src/views/Music.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: 'music'
+  name: 'Music'
 }
 </script>
