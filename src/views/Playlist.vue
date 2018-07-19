ffb07d3e849b5db8f1d6b5b4cf3aefd90e88584d
huntdream <maoyu960320@hotmail.com>
2018-07-19 22:08:39 +0800

    add playlist components

---
+++ src/views/Playlist.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>Play list here</div>
+</template>
+
+<script>
+  import {playlistdetail} from '../api/index'
+
+  export default {
+    name: 'Playlist',
+    data() {
+      return {
+        playlist: []
+      }
+    },
+    created() {
+      this.getplaylistdetail()
+    },
+    methods: {
+      getplaylistdetail() {
+        playlistdetail(this.$route.params.id).then(res => this.playlist = res)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
