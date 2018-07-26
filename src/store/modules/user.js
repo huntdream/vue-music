fecfbe963c7aa39dd475d22a1b42a59a57a022d0
huntdream <maoyu960320@hotmail.com>
2018-07-26 23:41:16 +0800

    add playlistcount for distinguishing  my own created and others playlist

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,7 +4,8 @@ export default {
   namespaced: true,
   state: {
     detail: [],
-    playlist: []
+    playlist: [],
+    playlistcount: 0
   },
   mutations: {
     saveDetail(state, detail) {
@@ -12,11 +13,17 @@ export default {
     },
     savePlaylist(state, playlist) {
       state.playlist = playlist
+    },
+    setPlaylistCount(state, count) {
+      state.playlistcount = count
     }
   },
   actions: {
     detail({ commit }) {
-      return userdetail().then(res => commit('saveDetail', res))
+      return userdetail().then(res => {
+        commit('saveDetail', res)
+        commit('setPlaylistCount', res.profile.playlistCount)
+      })
     },
     playlist({ commit }) {
       return userplaylist().then(res => commit('savePlaylist', res.playlist))
