362341705ce7e3a14907324754d2eefb816c6cd8
huntdream <maoyu960320@hotmail.com>
2018-07-28 23:14:23 +0800

    removeEventListener when component distroyed, change navbar backgroundcolor and active style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <header :class="{'top-nav':true,'no-transparent':this.isAlpha}">
-        <router-link to="/me" active-class="active" class="nav-item">
+        <router-link to="/" active-class="active" class="nav-item" exact>
             <span class="nav-icon me"></span>
         </router-link>
-        <router-link exact to="/" active-class="active" class="nav-item">
+        <router-link exact to="/music" active-class="active" class="nav-item">
             <span class="nav-icon music"></span>
         </router-link>
         <router-link to="/community" active-class="active" class="nav-item">
@@ -21,17 +21,20 @@ export default {
     }
   },
   created() {
-    window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', this.toggleAlpha)
+  },
+  methods: {
+    toggleAlpha() {
       let top = window.pageYOffset || document.documentElement.scrollTop
       if (top > 250) {
         this.isAlpha = true
       } else {
         this.isAlpha = false
       }
-    })
+    }
   },
-  methods: {
-    toggleAlpha() {}
+  destroyed() {
+    window.removeEventListener('scroll', this.toggleAlpha)
   }
 }
 </script>
@@ -46,22 +49,24 @@ export default {
         justify-content: center
         height: 3rem
         line-height: 3rem
-        background-color: rgba(162, 158, 230, 0.3)
+        background-color: rgba(0, 0, 0, 0.8)
         box-shadow: 0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 10px 0 rgba(0, 0, 0, .12);
         z-index: 999
 
         &.no-transparent
-            background-color: rgba(162, 158, 230, 1)
+            background-color: rgba(0, 0, 0, 1)
 
         .nav-item
             color: #fff
             font-weight: 500
+            transition: background-color .2s
 
             &:hover
                 background-color: #7d7ab5
 
             &.active
-                background-color: #7d7ab5
+                background-color: #000
+                box-shadow: inset 0px 0px 0px 1px #252b23
 
             .nav-icon
                 display: block
