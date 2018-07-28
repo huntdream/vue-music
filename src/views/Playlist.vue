9ff166f63d8394aea6eb44d1a369f4f1f4ac6c94
huntdream <maoyu960320@hotmail.com>
2018-07-28 23:15:06 +0800

    update

--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -1,14 +1,20 @@
-<template>
-    <div class="playlist-wrap">
-      <div class="playlist-name">{{playlistdetail.name}}</div>
+<template >
+    <div class="playlist-wrap" v-if="playlist.name">
+      <div class="playlist-detail-wrap">
+        <img :src="playlist.coverImgUrl" alt="playlist.name" />
+        <h1>{{playlist.name}}</h1>
+      </div>
     </div>
+    <spinner v-else></spinner>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Spinner from '@/components/Spinner'
 
 export default {
   name: 'Playlist',
+  components: { Spinner },
   data() {
     return {}
   },
@@ -17,12 +23,19 @@ export default {
   },
   computed: {
     ...mapState({
-      playlistdetail: state => state.playlist.detail
+      playlist: state => state.playlist.detail
     })
   },
   methods: {}
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+.playlist-wrap
+    .playlist-detail-wrap
+      img
+        width: 5rem
+        height: 5rem
+        border-radius: 5px
+        margin: 1rem
 </style>
