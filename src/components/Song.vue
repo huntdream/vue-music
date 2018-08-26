40a679215a62ec918566c245114e33808f738d64
huntdream <maoyu960320@hotmail.com>
2018-08-26 21:15:32 +0800

    Add song components

---
+++ src/components/Song.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="song" @click="play(id,name)">
+    {{name}}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Song',
+  props: ['name', 'id'],
+  computed: {
+    ...mapState({
+      detail: state => state.song.detail
+    })
+  },
+  methods: {
+    play(id, name) {
+      this.$store.dispatch('song/detail', { id, name });
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.song
+  padding: 1rem
+  border-top: 1px solid #eee
+</style>
