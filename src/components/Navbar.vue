08e743ce1db8dcfe070b0444aeec8c1123c114e9
huntdream <maoyu960320@hotmail.com>
2018-08-28 20:23:22 +0800

    style change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <header :class="{'top-nav':true,'no-transparent':this.isAlpha}">
+    <header class="top-nav">
         <router-link to="/" active-class="active" class="nav-item" exact>
             <span class="nav-icon me"></span>
         </router-link>
@@ -16,27 +16,12 @@
 export default {
   name: 'Navbar',
   data() {
-    return {
-      isAlpha: false
-    }
+    return {};
   },
-  created() {
-    window.addEventListener('scroll', this.toggleAlpha)
-  },
-  methods: {
-    toggleAlpha() {
-      let top = window.pageYOffset || document.documentElement.scrollTop
-      if (top > 250) {
-        this.isAlpha = true
-      } else {
-        this.isAlpha = false
-      }
-    }
-  },
-  destroyed() {
-    window.removeEventListener('scroll', this.toggleAlpha)
-  }
-}
+  created() {},
+  methods: {},
+  destroyed() {}
+};
 </script>
 
 <style lang="sass" scoped>
@@ -47,26 +32,15 @@ export default {
         display: flex
         flex-direction: row
         justify-content: center
-        height: 3rem
-        line-height: 3rem
-        background-color: rgba(0, 0, 0, 0.8)
+        height: 3.2rem
+        line-height: 3.2rem
+        background-color: rgba(4,67,98,.25)
         box-shadow: 0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 10px 0 rgba(0, 0, 0, .12);
         z-index: 999
-
-        &.no-transparent
-            background-color: rgba(0, 0, 0, 1)
 
         .nav-item
             color: #fff
             font-weight: 500
-            transition: background-color .2s
-
-            &:hover
-                background-color: #7d7ab5
-
-            &.active
-                background-color: #000
-                box-shadow: inset 0px 0px 0px 1px #252b23
 
             .nav-icon
                 display: block
