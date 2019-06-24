00e7d6e6c5ff9fe48e8abc9139f6c74584f787eb
Yu Mao <maoyu960320@hotmail.com>
2019-06-24 11:26:00 +0800

    update config

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,8 @@
-!usr/bin sh
+#!bin/sh
 
 echo 'Hello, this script will going build your app'
 npm run build
-dockerid="$(cat /etc/hostname)"
+pwd
 ls -al
-docker cp "${dockerid}":./dist /home/project
 sleep 1
 echo $! > .pidfile
