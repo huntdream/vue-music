d46ae2baa90f2c28eb82e910614bfa7325db9b9d
Yu Mao <maoyu960320@hotmail.com>
2019-06-24 11:19:39 +0800

    try to move files from container to host

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 echo 'Hello, this script will going build your app'
 npm run build
-cat /etc/hostname
-pwd
+dockerid="$(cat /etc/hostname)"
 ls -al
+docker cp "${dockerid}:./dist /home/project
 sleep 1
 echo $! > .pidfile
