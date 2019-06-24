cf162279acc60ad80d34d0c9141127801eb1546d
Yu Mao <maoyu960320@hotmail.com>
2019-06-24 11:21:16 +0800

    fix: Syntax error: Unterminated quoted string

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,6 +4,6 @@ echo 'Hello, this script will going build your app'
 npm run build
 dockerid="$(cat /etc/hostname)"
 ls -al
-docker cp "${dockerid}:./dist /home/project
+docker cp "${dockerid}":./dist /home/project
 sleep 1
 echo $! > .pidfile
